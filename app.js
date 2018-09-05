5cb3bf871da2496f026560d4392ac31b698c72af
MAY <cm0850068@otc.edu>
2018-09-05 09:36:32 -0500

    Getting it to work

---
+++ app.js
@@ -0,0 +1,19 @@
+function square1(x){
+    let name=x
+    x = Math.pow(x,2)
+    return x;
+
+
+}
+
+
+const square2=x=>Math.pow(x,2)
+
+
+console.log(square1(0));
+console.log(square1(2));
+console.log(square1(5));
+
+console.log(square2(0));
+console.log(square2(2));
+console.log(square2(5)); 
