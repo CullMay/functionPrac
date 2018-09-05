25048234ec59b1be285ed1256092dd089e3f5ad1
MAY <cm0850068@otc.edu>
2018-09-05 09:44:40 -0500

    2nd part done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function square1(x){
+/*function square1(x){
     let name=x
     x = Math.pow(x,2)
     return x;
@@ -16,4 +16,19 @@ console.log(square1(5));
 
 console.log(square2(0));
 console.log(square2(2));
-console.log(square2(5)); 
+console.log(square2(5));
+*/
+
+
+const min= (x,y)=>{
+    if(x>y)
+        return y;
+    if(x<y);
+        return x;
+    if(x=y)
+        return x;
+} 
+
+console.log(min(4.5, 5)); // Must show 4.5
+console.log(min(19, 9));  // Must show 9
+console.log(min(1, 1));   // Must show 1
